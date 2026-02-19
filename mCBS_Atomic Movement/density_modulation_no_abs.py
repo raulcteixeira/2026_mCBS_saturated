f1988f341ad389ef3e41e873b3ac72d504f35118
raulcteixeira <teixeira@df.ufscar.br>
2026-02-19 16:22:58 +0100

    add file with no abs

--- mCBS_Atomic Movement/density_modulation_no_abs.py
+++ mCBS_Atomic Movement/density_modulation_no_abs.py
@@ -11,23 +11,14 @@ def local_s(s,z,k_vec):
     
     return s_l
 
-def photon_drag(s,z,k_vec,sqr_alph):
-    
-    ph_drag = s*((1-sqr_alph)**2 + 2*sqr_alph*(1-sqr_alph)*(np.cos(k_vec*z))**2)
-    
-    return ph_drag
-
 
 import numpy as np
 import matplotlib.pyplot as plt
 import random
 
+s_in = 5
 lambda0 = 461e-9;
 k = 2*np.pi/lambda0
-
-s_in = 1.0        # saturation parameter of incoming beam
-alpha = 1.0         # power of reflected beam relative to incoming
-sqr_alpha = np.sqrt(alpha)
 
 m = 87.9 * 1.66e-27 
 hbar = 6.63e-34/2/np.pi
@@ -61,19 +52,16 @@ for ii in range(Nat):
         T_limit= (jj+1)*T_step
         while time < T_limit:
             s_now = local_s(s_in,k,x)
-            # sp_em_rate: spontaneous emission rate
-            sp_em_rate = 1/2 * G_Sr * s_now/(1 + s_now) 
-            # photon_abs_rate: average force from beam imbalance
-            photon_abs_rate = 1/2 * G_Sr * photon_drag(s_in,k,x,sqr_alpha)/(1 + s_now)
+            photon_rate = 1/2 * G_Sr * s_now/(s_now+1)
+            #print (delta_t)
+            #print (v*delta_t/lambda0)
             time = time + dt
             x = x + v*dt
             sort = random.random()
-            if sort < 1/6*sp_em_rate*dt:
+            if sort < 1/6*photon_rate*dt:
                 v = v + vr
-            elif sort < 1/3*sp_em_rate*dt:
+            elif sort < 1/3*photon_rate*dt:
                 v = v - vr
-            elif sort > (1 - photon_abs_rate*dt):   # average force from beam imbalance
-                v = v + vr
         x_index = int((x/lambda0*N_x)%N_x)
         final_x[x_index,jj+1] = final_x[x_index,jj+1] + 1
 
@@ -84,33 +72,19 @@ pop_ex = local_s(s_in,k,x_vector)/(local_s(s_in,k,x_vector)+1)
 
 time_interval = 1
 
-plt.figure(1)
 for jj in range(int((N_steps_T-1)/time_interval)+1):
     time_index = jj*time_interval
     plt.plot(x_vector/lambda0,final_x[:,time_index],label=str(round(time_index*T_step*1e6,1))+' us')
 plt.plot(x_vector/lambda0,Nat/N_x*pop_ex,label = 'population')
-plt.title('s = 1.0, alpha = 0.9')
+plt.title('s = 5')
 plt.legend()
 plt.xlabel('Position (lambda)')
 plt.ylabel('Frequency')
 
-#%%
-
-time_interval = 2
-
-plt.figure(2)
-for jj in range(int((N_steps_T-1)/time_interval)+1):
-    time_index = jj*time_interval
-    plt.plot(x_vector/lambda0,final_x[:,time_index],label=str(round(time_index*T_step*1e6,1))+' us')
-plt.plot(x_vector/lambda0,Nat/N_x*pop_ex,label = 'population')
-plt.title('s = 1.0, alpha = 0.9')
-plt.legend()
-plt.xlabel('Position (lambda)')
-plt.ylabel('Frequency')
 
 with open('test.npy', 'wb') as f:
 
-    np.save(f, final_x)
+    np.save(f, np.array([1, 2]))
 
 
 # plt.hist(final_x[:,0],bins = 20,histtype='step',label = 't = 0')
@@ -124,3 +98,20 @@ with open('test.npy', 'wb') as f:
 # plt.hist(final_x[:,20],bins = 20,histtype='step',label = 't = 2us')
 # plt.title('s = 5')
 # plt.legend()
+
+#%%
+
+time_interval = 2
+
+for jj in range(int((N_steps_T-1)/time_interval)+1):
+    time_index = jj*time_interval
+    plt.plot(x_vector/lambda0,final_x[:,time_index],label=str(round(time_index*T_step*1e6,1))+' us')
+plt.plot(x_vector/lambda0,Nat/N_x*pop_ex,label = 'population')
+plt.title('s = 5')
+plt.legend()
+plt.xlabel('Position (lambda)')
+plt.ylabel('Frequency')
+
+with open('test.npy', 'wb') as f:
+
+    np.save(f, final_x)
